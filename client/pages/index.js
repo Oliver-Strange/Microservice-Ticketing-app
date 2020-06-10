9285cafc898d727f42476d90486c910180a67557
Oliver-Strange <preston.burton@gmail.com>
2020-06-10 16:17:01 -0700

    adds useRequest helper hook, gitInitialProps to index

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,23 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
   return <h1>it's working!</h1>;
 };
+
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    const response = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers,
+      }
+    );
+    return response.data;
+  } else {
+    const response = await axios.get('/api/users/currentuser');
+    return response.data;
+  }
+  return {};
+};
+
+export default LandingPage;
