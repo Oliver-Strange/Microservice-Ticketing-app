bc6e71796886eae07e4bcf43de79804d1132b0d3
Oliver-Strange <preston.burton@gmail.com>
2020-06-13 17:09:48 -0700

    adds initial props to app component, adds header, adds signout page

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,9 +1,35 @@
+import buildClient from '../api/build-client';
+import Header from '../components/header';
+
 // adds global css to project
 import 'bootstrap/dist/css/bootstrap.css';
 // global css needs to be used in the app file, but nextjs doesn't use one
 
 // this wraps page as components passing in the pageProps
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
 };
 // nextjs passes each page to this app file as the component, allowing the global css to work
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  let pageProps = {};
+  // git initial props for landing page
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return {
+    pageProps,
+    ...data,
+  };
+};
+
+export default AppComponent;
