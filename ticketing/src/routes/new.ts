ce0d260494a89ca114fa1c688836083b349d8513
Oliver-Strange <preston.burton@gmail.com>
2020-06-22 11:41:16 -0700

    adds ticket creation and testing for it

--- ticketing/src/routes/new.ts
+++ ticketing/src/routes/new.ts
@@ -1,10 +1,33 @@
 import express, { Request, Response } from 'express';
-import { requireAuth } from '@osorg/common-middleware';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@osorg/common-middleware';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-router.post('/api/ticketing', requireAuth, (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
+router.post(
+  '/api/ticketing',
+  requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than zero'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    // pull out title and price from incoming request
+    const { title, price } = req.body;
+
+    const ticket = Ticket.build({
+      title,
+      price,
+      // ts complains that current user might not be defined but require auth will stop
+      // the whole process before current user would be needed
+      userId: req.currentUser!.id,
+    });
+    await ticket.save();
+
+    res.status(201).send(ticket);
+  }
+);
 
 export { router as createTicketRouter };
