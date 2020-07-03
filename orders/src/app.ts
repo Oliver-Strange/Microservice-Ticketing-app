4d688d05b134ed8f3b700f549fae42432b2bdad3
Oliver-Strange <preston.burton@gmail.com>
2020-07-03 09:03:24 -0700

    starting the orders service

---
+++ orders/src/app.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { errorHandler, NotFoundError, currentUser } from '@osorg/common-middleware';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+// checks incoming request for route that doesn't exist
+// throwing a new error for the errorHandler to deal with
+app.all('*', () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
