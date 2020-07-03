c6f7734cfae6ddd28e510610c58cc38ff5336a90
Oliver-Strange <preston.burton@gmail.com>
2020-07-03 10:43:05 -0700

    sets up order schema and validation for new order

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,25 @@
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@osorg/common-middleware';
+import mongoose from 'mongoose';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      // body check to make sure the incoming id is a valid mongodb id
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('TicketId must be provided'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
