333d3c5a43053d3e67d82ee1810fae7512dcde54
Oliver-Strange <preston.burton@gmail.com>
2020-07-07 07:54:16 -0700

    adds ticket id to all test in orders routes

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -3,12 +3,14 @@ import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
 import { Order, OrderStatus } from '../../models/order';
 import { natsWrapper } from '../../nats-wrapper';
+import mongoose from 'mongoose';
 
 it('marks an order as cancelled', async () => {
   // create a ticket with Ticket model
   const ticket = Ticket.build({
     title: 'aTitle',
     price: 20,
+    id: new mongoose.Types.ObjectId().toHexString(),
   });
   await ticket.save();
 
@@ -33,6 +35,7 @@ it('emits an event saying the order was cancelled', async () => {
   const ticket = Ticket.build({
     title: 'aTitle',
     price: 20,
+    id: new mongoose.Types.ObjectId().toHexString(),
   });
   await ticket.save();
 
