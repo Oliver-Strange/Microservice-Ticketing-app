c42eb7d0830ca44ed445b981dc2ccf33e076f939
Oliver-Strange <preston.burton@gmail.com>
2020-07-08 12:24:25 -0700

    create src, copies nats, mock, and index from ticketing

---
+++ expiration/src/__mocks__/nats-wrapper.ts
@@ -0,0 +1,8 @@
+// fake nats-wrapper
+export const natsWrapper = {
+  client: {
+    publish: jest.fn().mockImplementation((subject: string, data: string, callback: () => void) => {
+      callback();
+    }),
+  },
+};
