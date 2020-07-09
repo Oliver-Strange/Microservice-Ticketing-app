4cf501ef967f7b1583a56b7e92645b1aff747786
Oliver-Strange <preston.burton@gmail.com>
2020-07-08 17:53:05 -0700

    updates deps, finds/fixes bug in listener, all tests pass

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -17,16 +17,16 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
     order.set({
       status: OrderStatus.Cancelled,
     });
-    await order.save()
+    await order.save();
 
     await new OrderCancelledPublisher(this.client).publish({
       id: order.id,
       version: order.version,
       ticket: {
-        id: order.ticket.id
-      };
-    })
+        id: order.ticket.id,
+      },
+    });
 
-    msg.ack()
+    msg.ack();
   }
 }
