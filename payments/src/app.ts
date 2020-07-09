66008b7c008d30db91aa2824e8dd02d6943a8504
Oliver-Strange <preston.burton@gmail.com>
2020-07-08 18:32:12 -0700

    setup basic payment structure

---
+++ payments/src/app.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { errorHandler, NotFoundError, currentUser } from '@osorg/common-middleware';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+app.use(currentUser);
+
+// checks incoming request for route that doesn't exist
+// throwing a new error for the errorHandler to deal with
+app.all('*', () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
