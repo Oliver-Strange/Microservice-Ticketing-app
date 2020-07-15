8be6a002eb877f680a6cd96e7a13c1f9c9d16cb7
OliverStrange <preston.burton@gmail.com>
2020-07-15 12:58:37 -0700

    adds create charge route to payments app.ts

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -1,25 +1,31 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
-import cookieSession from 'cookie-session';
-
-import { errorHandler, NotFoundError, currentUser } from '@osorg/common-middleware';
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { createChargeRouter } from "./routes/new";
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from "@osorg/common-middleware";
 
 const app = express();
-app.set('trust proxy', true);
+app.set("trust proxy", true);
 app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== 'test',
+    secure: process.env.NODE_ENV !== "test",
   })
 );
 
 app.use(currentUser);
 
+app.use(createChargeRouter);
+
 // checks incoming request for route that doesn't exist
 // throwing a new error for the errorHandler to deal with
-app.all('*', () => {
+app.all("*", () => {
   throw new NotFoundError();
 });
 
