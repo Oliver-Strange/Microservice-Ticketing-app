763aa9ebbb79f6501abd1e05641225593b546485
OliverStrange <preston.burton@gmail.com>
2020-07-15 14:56:19 -0700

    tests mock version of stripe charge create, updates new charge route to send 201 status

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -3,6 +3,9 @@ import { app } from "../../app";
 import mongoose from "mongoose";
 import { Order } from "../../models/order";
 import { OrderStatus } from "@osorg/common-middleware";
+import { stripe } from "../../stripe";
+
+jest.mock("../../stripe.ts");
 
 it("returns a 404 when purchasing an order that does not exist", async () => {
   await request(app)
@@ -55,4 +58,32 @@ it("returns a 400 when purchasing a cancelled order", async () => {
       orderId: order.id,
     })
     .expect(400);
+});
+
+it("returns a 204 with valid inputs", async () => {
+  const userId = mongoose.Types.ObjectId().toHexString();
+
+  const order = Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    userId,
+    version: 0,
+    price: 20,
+    status: OrderStatus.Created,
+  });
+  await order.save();
+
+  await request(app)
+    .post("/api/payments")
+    .set("Cookie", global.signin(userId))
+    .send({
+      token: "tok_visa",
+      orderId: order.id,
+    })
+    .expect(201);
+
+  // access the chargeOptions by going into the array of arrays first time it was called and it's first argument
+  const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+  expect(chargeOptions.source).toEqual("tok_visa");
+  expect(chargeOptions.amount).toEqual(20 * 100);
+  expect(chargeOptions.currency).toEqual("usd");
 });
