4ca5a89fc4e2f346915318be501f840c4ad79a9e
OliverStrange <preston.burton@gmail.com>
2020-07-15 11:11:38 -0700

    runs npm install, sets up order model for payments

---
+++ payments/src/models/order.ts
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+import { OrderStatus } from "@osorg/common-middleware";
+
+interface OrdersAttrs {
+  id: string;
+  version: number;
+  userId: string;
+  price: number;
+  status: OrderStatus;
+}
+
+interface OrderDoc extends mongoose.Document {
+  version: number;
+  userId: string;
+  price: number;
+  status: OrderStatus;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrdersAttrs): OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+orderSchema.statics.build = (attrs: OrdersAttrs) => {
+  return new Order({
+    _id: attrs.id,
+    version: attrs.version,
+    price: attrs.price,
+    userId: attrs.userId,
+    status: attrs.status,
+  });
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>("Order", orderSchema);
+
+export { Order };
