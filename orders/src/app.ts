7a4e6b833cc73be4b172ecf750cdf0851a5b5954
OliverStrange <preston.burton@gmail.com>
2020-07-20 13:55:31 -0700

    https update

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -1,21 +1,25 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
-import cookieSession from 'cookie-session';
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
 
-import { errorHandler, NotFoundError, currentUser } from '@osorg/common-middleware';
-import { newOrderRouter } from './routes/new';
-import { showOrderRouter } from './routes/show';
-import { indexOrderRouter } from './routes/index';
-import { deleteOrderRouter } from './routes/delete';
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from "@osorg/common-middleware";
+import { newOrderRouter } from "./routes/new";
+import { showOrderRouter } from "./routes/show";
+import { indexOrderRouter } from "./routes/index";
+import { deleteOrderRouter } from "./routes/delete";
 
 const app = express();
-app.set('trust proxy', true);
+app.set("trust proxy", true);
 app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== 'test',
+    secure: false,
   })
 );
 
@@ -28,7 +32,7 @@ app.use(deleteOrderRouter);
 
 // checks incoming request for route that doesn't exist
 // throwing a new error for the errorHandler to deal with
-app.all('*', () => {
+app.all("*", () => {
   throw new NotFoundError();
 });
 
--- ticketing/src/app.ts
+++ ticketing/src/app.ts
@@ -1,21 +1,25 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
-import cookieSession from 'cookie-session';
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
 
-import { errorHandler, NotFoundError, currentUser } from '@osorg/common-middleware';
-import { createTicketRouter } from './routes/new';
-import { showTicketRouter } from './routes/show';
-import { indexTicketRouter } from './routes/index';
-import { updateTicketRouter } from './routes/update';
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from "@osorg/common-middleware";
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes/index";
+import { updateTicketRouter } from "./routes/update";
 
 const app = express();
-app.set('trust proxy', true);
+app.set("trust proxy", true);
 app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== 'test',
+    secure: false,
   })
 );
 
@@ -28,7 +32,7 @@ app.use(updateTicketRouter);
 
 // checks incoming request for route that doesn't exist
 // throwing a new error for the errorHandler to deal with
-app.all('*', () => {
+app.all("*", () => {
   throw new NotFoundError();
 });
 
