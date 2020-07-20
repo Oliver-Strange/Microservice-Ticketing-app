e15e016a5ec30a5851cf2698eafb228fe16da7a9
OliverStrange <preston.burton@gmail.com>
2020-07-20 12:09:26 -0700

    another small change

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,25 +1,26 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
-import cookieSession from 'cookie-session';
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
 
-import { currentUserRouter } from './routes/current-user';
-import { signupRouter } from './routes/signup';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
+import { currentUserRouter } from "./routes/current-user";
+import { signupRouter } from "./routes/signup";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
 
-import { errorHandler, NotFoundError } from '@osorg/common-middleware';
+import { errorHandler, NotFoundError } from "@osorg/common-middleware";
 
 const app = express();
-app.set('trust proxy', true);
+app.set("trust proxy", true);
 app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== 'test',
+    secure: process.env.NODE_ENV !== "test",
   })
 );
 
+// Routes
 app.use(currentUserRouter);
 app.use(signupRouter);
 app.use(signinRouter);
@@ -27,7 +28,7 @@ app.use(signoutRouter);
 
 // checks incoming request for route that doesn't exist
 // throwing a new error for the errorHandler to deal with
-app.all('*', () => {
+app.all("*", () => {
   throw new NotFoundError();
 });
 
