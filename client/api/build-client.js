27621f76b996df987c240782bdc59476acdc9e7e
OliverStrange <preston.burton@gmail.com>
2020-07-21 12:39:12 -0700

    forgot slash

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,7 @@ export default ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL: "http://www.ticket-online-prod.page",
+      baseURL: "http://www.ticket-online-prod.page/",
       headers: req.headers,
     });
   } else {
